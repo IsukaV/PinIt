af64b62bc50a14594c043380abeca0ad816a7bde
IsukaV <isukapremathilake@gmail.com>
2024-11-05 23:28:33 +0530

    Task date and heading changes

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -4,16 +4,23 @@ function Task({Data}) {
 
     console.log(Data)
   return (
-    <div className="grid grid-cols-[92%,3%] gap-[5%] w-[400px] h-[150px] bg-[#ffffff] rounded-[15px] overflow-hidden">
+    <div className="grid grid-cols-[97%,3%] w-[400px] h-[130px] bg-[#ffffff] rounded-[15px] overflow-hidden">
         <div className='flex justify-between'>
             <div className='w-[60px]'>
             <img src="/assets/images/Starred.png" className="h-[21px] mt-[18px] cursor-pointer m-auto hover:h-[23px] hover:mt-[17px]"/>
             </div>
             
-            <div className='w-[100%] pt-[18px] '>
-                <div className='font-medium'>
+            <div className='w-[69%] pt-[18px] '>
+                <div className='font-medium break-words line-clamp-2'>
                 {Data.Heading}
                 </div>
+                <div className='font-light text-[15px] pt-[10px]'>
+                    {Data.Date}
+                </div>
+            </div>
+
+            <div className='w-[60px]'>
+            <img src="/assets/images/3dots.png" className="h-[21px] mt-[18px] cursor-pointer m-auto hover:h-[23px] hover:mt-[17px]"/>
             </div>
 
         </div>
--- frontend/src/components/Today.js
+++ frontend/src/components/Today.js
@@ -5,6 +5,10 @@ function Today() {
   const [date] = useState(new Date());
 
   const data={
+    Heading: "Design the task comonents comonents comonents comonents qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq",
+    Date: '12-43-23'
+  }
+  const data1={
     Heading: "Design the task comonents",
     Date: '12-43-23'
   }
@@ -16,15 +20,28 @@ function Today() {
 
   return (
     <div className='p-[40px] '>
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <div>
           <p className="text-[40px]">Today,</p>
           <p  className="font-light ml-1">{formattedDate}</p>
         </div>
+
+        <div className='bg-[#ffffff] w-[370px] h-[70px] flex justify-around rounded-[20px]'>
+          <div className='flex gap-[20px] items-center'>
+            <div className='bg-[#0C7DFF] w-[30px] h-[30px] rounded-[5px]'></div>
+            <p className=''>Personal</p>
+          </div>
+
+          <div className='flex gap-[20px] items-center'>
+            <div className='bg-[#F4A105] w-[30px] h-[30px] rounded-[5px]'></div>
+            <p className=''>Group</p>
+          </div>
+
+        </div>
       </div>
       <div className="flex flex-row justify-between mt-[40px]">
         <Task Data={data}/>
-        <Task Data={data}/>
+        <Task Data={data1}/>
         <Task Data={data}/>
       </div>
     </div>
